12c754262d7819e3de266bfd8fc0ed80611dda81
WilsonFung414 <wilsonfung414@gmail.com>
2022-04-14 23:32:49 +0800

    Update common.py

--- cogs/common.py
+++ cogs/common.py
@@ -18,6 +18,7 @@ class common(commands.Cog):
 
     @commands.command()
     async def jp(self, ctx):
-        await ctx.send(f'あ　い　う　え　お\nか　き　く　け　こ\nさ　し　す　せ　そ\nた　ち　つ　て　と\nな　に　ぬ　ね　の\nは　ひ　ふ　へ　ほ\nま　み　む　め　も\nや　　　ゆ　　　よ\nら　り　る　れ　ろ\nわ　を\nん\nWah')
+        await ctx.send(f'あ　い　う　え　お\nか　き　く　け　こ\nさ　し　す　せ　そ\nた　ち　つ　て　と\nな　に　ぬ　ね　の\nは　ひ　ふ　へ　ほ\n\
+                        ま　み　む　め　も\nや　　　ゆ　　　よ\nら　り　る　れ　ろ\nわ　を\nん\nWah')
 def setup(client):
     client.add_cog(common(client))
